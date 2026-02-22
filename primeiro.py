5789dde8bb9aca6be419d46777b4cc6c1772569c
lucasnotaroberto2-oss <lucasnotaroberto2@gmail.com>
2026-02-22 00:06:23 -0300

    Add files via upload

---
+++ primeiro.py
@@ -0,0 +1,6 @@
+s = float(input("digite seu salario atual:"))
+if s < 1800:
+    s2 = s * 1.1
+    print("parabens, voce recebeu um aumento de 10%, portanto seu novo salario é: ",s2)
+else:
+    print("voce não tem direito a um aumento de salario, atura ou surta")
